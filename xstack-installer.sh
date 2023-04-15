2014b6f57b709fac753d8989077c06a84f953cbf
lilUnkelDemon <amir.momeni.gelloria79@gmail.com>
2023-04-15 14:56:00 +0330

    v6

--- xstack-installer.sh
+++ xstack-installer.sh
@@ -126,7 +126,11 @@ install_API_Panel() {
 
 
                 progress
-                SED=$(sed -i "s/\$fileAddress = "defval";/\$fileAddress = \"${FILE}\";/" config.php)
+                # SED=$(sed -i "s/\$fileAddress = "defval";/\$fileAddress = \"$FILE\";/" config.php)
+                # SED=$(sed -i "s/\$fileAddress = .*/\$fileAddress = \"$FILE\";/" config.php)
+                # SED=$(grep -q -F '$fileAddress =' config.php && sed -i 's/\$fileAddress = .*/\$fileAddress = "dsdsdsds";/' config.php || echo '$fileAddress not found!')
+                SED=$(grep -q -F '$fileAddress =' config.php && sed -i "s/\(\$fileAddress = \).*/\1\"$FILE\";/" config.php || echo '$fileAddress not found!')
+                
                 echo $SED
 
 

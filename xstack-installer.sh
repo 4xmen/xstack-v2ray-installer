b25ddbb6d62451ea7f947599e308216f1312716d
lilUnkelDemon <amir.momeni.gelloria79@gmail.com>
2023-04-15 14:48:31 +0330

    v3

--- xstack-installer.sh
+++ xstack-installer.sh
@@ -126,7 +126,7 @@ install_API_Panel() {
 
 
                 progress
-                SED=sed -i 's/defval/${FILE}/g' /var/www/html/config.php
+                SED=$(sed -i 's/\$fileAddress = "defval";/\$fileAddress = "${FILE}";/' config.php)
                 echo $SED
 
 
